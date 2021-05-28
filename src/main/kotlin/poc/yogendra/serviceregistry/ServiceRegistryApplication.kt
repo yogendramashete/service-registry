c6ed1fe42c7e00b91e22611bb80758d7bd44a6df
yogendramashete <yogendra.mashete@gmail.com>
2021-05-28 15:00:57 +0530

    Initial commit

---
+++ src/main/kotlin/poc/yogendra/serviceregistry/ServiceRegistryApplication.kt
@@ -0,0 +1,13 @@
+package poc.yogendra.serviceregistry
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer
+
+@SpringBootApplication
+@EnableEurekaServer
+class ServiceRegistryApplication
+
+fun main(args: Array<String>) {
+	runApplication<ServiceRegistryApplication>(*args)
+}
